d680dad0f420c293ee98076afcd0fe43e83861e8
Tinny <mbothepha@gmail.com>
2025-07-23 19:49:12 +0200

    update page.tsx

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,17 +5,15 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
- 
-export default async function Page(props: {
-  searchParams?: Promise<{
-    query?: string;
-    page?: string;
-  }>;
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { query?: string; page?: string };
 }) {
-  const searchParams = await props.searchParams;
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
- 
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
