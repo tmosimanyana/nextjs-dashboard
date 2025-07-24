30da7cadaa036452bbd7ca7db4f24fed726a3bb8
Tinny <mbothepha@gmail.com>
2025-07-24 15:09:50 +0200

    update panigation.tsx

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -5,17 +5,23 @@ import {
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
-import Link from 'next/link'; // ✅ Correct import
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 
-// Define the links array
 const links = [
-  { name: 'Home', href: '/', icon: HomeIcon },
-  { name: 'Customers', href: '/customers', icon: UserGroupIcon },
-  { name: 'Documents', href: '/documents', icon: DocumentDuplicateIcon },
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
@@ -24,7 +30,12 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
@@ -34,4 +45,3 @@ export default function NavLinks() {
     </>
   );
 }
-
