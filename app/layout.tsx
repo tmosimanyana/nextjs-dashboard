71730777f2f5b1b0e43e6f76951acf365fc6a46d
Tinny <mbothepha@gmail.com>
2025-07-24 21:35:49 +0200

    update files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+// app/layout.tsx
+
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
- 
+import { Providers } from './providers';
+
 export default function RootLayout({
   children,
 }: {
@@ -8,7 +11,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
